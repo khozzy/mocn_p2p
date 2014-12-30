a03198c93e8a6f055d3e7297d155eeda7b71a260
Norbert Kozłowski <norbert.kozlowski@intenso.com.pl>
2014-12-30 15:22:39 +0100

    Build configuration

--- src/com/pwr/Main.java
+++ src/com/pwr/Main.java
@@ -1,8 +1,21 @@
 package com.pwr;
 
+import ilog.concert.IloException;
+import ilog.cplex.IloCplex;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from here :)");
+
+        System.out.println("Welcome. I'm your wizard in the dark lands of P2P networks...");
+        System.out.println("State your problem, mortal ...");
+
+        try {
+            IloCplex cplex = new IloCplex();
+            // create model and solve it
+        } catch (IloException ex) {
+            System.err.println("Ugh, I got hurt: " + ex);
+        }
+
     }
 }
