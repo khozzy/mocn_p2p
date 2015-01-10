a9c777a7d710b8e5164f83389896e3b3eb781303
Norbert <khozzy@gmail.com>
2015-01-10 10:37:17 +0100

    Move evertything into Simulation

--- src/com/pwr/Simulation.java
+++ src/com/pwr/Simulation.java
@@ -1,56 +1,165 @@
 package com.pwr;
 
+import ilog.concert.IloException;
+import ilog.concert.IloIntExpr;
+import ilog.concert.IloLinearNumExpr;
+import ilog.concert.IloNumVar;
+import ilog.cplex.IloCplex;
+
 public class Simulation {
 
-    private final int NODES = 10;
-    private final int BLOCKS_TO_TRANSFER = 1;
-    private final int MAX_TIME = 20;
+    public double run() throws IloException {
 
-    private Network network = new Network(NODES);
+        final int NODES = 5;
+        final int BLOCKS_TO_TRANSFER = 1;
+        final int MAX_TIME = 2;
+        final int M = 6;
 
-    // g_bv if block b is located on node v before transfer starts
-    private boolean[][] nodeHasBlockBeforeTransfer = new boolean[BLOCKS_TO_TRANSFER][NODES];
+        double objValue = 0.0;
+        final double[][][][] solution = new double[BLOCKS_TO_TRANSFER][NODES][NODES][MAX_TIME];
 
-    // y_bwvt block b transferred from w -> v in time t
-    private boolean[][][][] blockTransferred = new boolean[BLOCKS_TO_TRANSFER][NODES][NODES][MAX_TIME];
-
-    public void start() {
-        wipeAllInformations();
-
-        System.out.println("[*] Network details");
-        network.displayNodeInfo();
-
-        System.out.println("[*] One node has a block of data");
-        int initNode = Utils.randInt(0, NODES - 1);
-        nodeHasBlockBeforeTransfer[0][initNode] = true;
-
-        System.out.println("[*] Node with number " + initNode + " chosen");
-        System.out.println("[*] Beginning the simulation");
-
-        for (int time = 0; time < MAX_TIME; time++) {
-            System.out.println("\tt = " + time);
-            // ...
+        //g_bv - czy node posiada blok przed startem czy nie
+        boolean[][] g = new boolean[BLOCKS_TO_TRANSFER][NODES];
+        //w naszym wypadku każdy blok ma jednego initial seeda
+        int initNode[] = new int[BLOCKS_TO_TRANSFER];
+        for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+            initNode[b] = Utils.randInt(0, NODES - 1);
+            g[b][initNode[b]] = true;
         }
 
-    }
+        IloCplex cplex = new IloCplex();
 
-    private void wipeAllInformations() {
-        System.out.println("[*] Wiping g_bv");
-        for (int block = 0; block < BLOCKS_TO_TRANSFER; block++) {
-            for (int node = 0; node < NODES; node++) {
-                nodeHasBlockBeforeTransfer[block][node] = false;
+        Network network = new Network(NODES);
+
+        //y_bwvt variable
+        IloNumVar[][][][] y = new IloNumVar[BLOCKS_TO_TRANSFER][NODES][NODES][MAX_TIME];
+        for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+            for (int w = 0; w < NODES; w++) {
+                for (int v = 0; v < NODES; v++) {
+                    y[b][w][v] = cplex.boolVarArray(MAX_TIME);
+                }
             }
         }
 
-        System.out.println("[*] Wiping y_bwvt");
-        for (int block = 0; block < BLOCKS_TO_TRANSFER; block++) {
-            for (int node_W = 0; node_W < NODES; node_W++) {
-                for (int node_V = 0; node_V < NODES; node_V++) {
-                    for (int time = 0; time < MAX_TIME; time++) {
-                        blockTransferred[block][node_W][node_V][time] = false;
+        //Objective function
+        IloLinearNumExpr objectiveFunction = cplex.linearNumExpr();
+        for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+            for (int w = 0; w < NODES; w++) {
+                for (int v = 0; v < NODES; v++) {
+                    for (int t = 0; t < MAX_TIME; t++) {
+                        //suma iloczynów kosztu transferu z node w do v i zmiennej y okreslajacej czy
+                        //w danej iteracji byl transfer danego bloku z node w do v czy nie
+                        objectiveFunction.addTerm(network.getCost(w, v), y[b][w][v][t]);
                     }
                 }
             }
         }
+        cplex.addMinimize(objectiveFunction);
+
+        //warunek 1 - wszystkie node'y muszą otrzymać pakiet - chyba dobrze
+        for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+            for (int v = 0; v < NODES; v++) {
+                IloLinearNumExpr sumOfY_bwvtOver_wt = cplex.linearNumExpr();
+                for (int w = 0; w < NODES; w++) {
+                    for (int t = 0; t < MAX_TIME; t++) {
+                        sumOfY_bwvtOver_wt.addTerm(1, y[b][w][v][t]);
+                    }
+                }
+                int myInt = (g[b][v]) ? 1 : 0;
+                IloIntExpr g_bv = cplex.constant(myInt);
+                cplex.addEq(cplex.sum(sumOfY_bwvtOver_wt, g_bv), 1);
+            }
+        }
+
+
+        //warunek 2 - upload constraint, chyba dobziu
+        for (int w = 0; w < NODES; w++) {
+            for (int t = 0; t < MAX_TIME; t++) {
+                IloLinearNumExpr sumOfY_bwvtOver_bv = cplex.linearNumExpr();
+                for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+                    for (int v = 0; v < NODES; v++) {
+                        sumOfY_bwvtOver_bv.addTerm(1, y[b][w][v][t]);
+                    }
+                }
+                cplex.addLe(sumOfY_bwvtOver_bv, network.getUploadRate(w));
+            }
+        }
+
+        //warunek 3 - download constraint
+        for (int v = 0; v < NODES; v++) {
+            for (int t = 0; t < MAX_TIME; t++) {
+                IloLinearNumExpr sumOfY_bwvtOver_bw = cplex.linearNumExpr();
+                for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+                    for (int w = 0; w < NODES; w++) {
+                        sumOfY_bwvtOver_bw.addTerm(1, y[b][w][v][t]);
+                    }
+                }
+                cplex.addLe(sumOfY_bwvtOver_bw, network.getDownloadRate(v));
+            }
+        }
+
+        //warunek 4 - blok moze zostac wyslany tylko jesli node go posiada
+        for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+            for (int w = 0; w < NODES; w++) {
+                for (int t = 0; t < MAX_TIME; t++) {
+                    IloLinearNumExpr sumOfY_bwvtOver_v = cplex.linearNumExpr();
+                    for (int v = 0; v < NODES; v++) {
+                        sumOfY_bwvtOver_v.addTerm(1, y[b][w][v][t]);
+                    }
+
+                    IloLinearNumExpr sumOfY_bswiOver_is = cplex.linearNumExpr();
+
+                    for (int i = 0; i < t; i++) {
+                        for (int s = 0; s < NODES; s++) {
+                            sumOfY_bswiOver_is.addTerm(1, y[b][s][w][i]);
+                        }
+                    }
+
+                    int myInt = (g[b][w]) ? 1 : 0;
+                    IloIntExpr g_bw = cplex.constant(myInt);
+
+                    cplex.addLe(sumOfY_bwvtOver_v, cplex.prod(M, cplex.sum(sumOfY_bswiOver_is, g_bw)));
+
+                }
+            }
+        }
+
+        //no i rozwiązujemy
+        if (cplex.solve()) {
+
+            objValue = cplex.getObjValue();
+
+            cplex.output().println("Solution status: " + cplex.getStatus());
+            cplex.output().println("Solution value: " + objValue);
+
+
+            for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+                for (int w = 0; w < NODES; w++) {
+                    for (int v = 0; v < NODES; v++) {
+                        solution[b][w][v] = cplex.getValues(y[b][w][v]);
+                    }
+                }
+            }
+
+            network.displayNodeInfo();
+            for (int b = 0; b < BLOCKS_TO_TRANSFER; b++) {
+                cplex.output().println("Block=" + b + " initially in node: " + initNode[b]);
+                for (int t = 0; t < MAX_TIME; t++) {
+                    for (int w = 0; w < NODES; w++) {
+                        for (int v = 0; v < NODES; v++) {
+                            if (Math.round(solution[b][w][v][t]) == 1) {
+                                cplex.output().println("Block=" + b + " in iteration=" + t + " transferred from node w="
+                                        + w + " to node v=" + v);
+                            }
+                        }
+                    }
+                }
+            }
+
+            cplex.end();
+        }
+
+        return objValue;
     }
+
 }
